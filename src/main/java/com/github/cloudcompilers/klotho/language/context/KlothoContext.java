4705a148b6ce7eee8744fd5be568d39eed5643a3
DavidSeptimus-Klotho <david.septimus@klo.dev>
2022-10-08 02:39:43 -0400

    Adds Live Templates for TS/JS and Python

---
+++ src/main/java/com/github/cloudcompilers/klotho/language/context/KlothoContext.java
@@ -0,0 +1,18 @@
+package com.github.cloudcompilers.klotho.language.context;
+
+import com.intellij.codeInsight.template.TemplateActionContext;
+import com.intellij.codeInsight.template.TemplateContextType;
+import org.jetbrains.annotations.NotNull;
+
+public class KlothoContext extends TemplateContextType {
+
+  protected KlothoContext() {
+    super("KLOTHO", "Klotho");
+  }
+
+  @Override
+  public boolean isInContext(@NotNull TemplateActionContext templateActionContext) {
+    return templateActionContext.getFile().getName().endsWith(".klotho");
+  }
+
+}
